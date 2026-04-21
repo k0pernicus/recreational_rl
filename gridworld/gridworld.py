941b95708f452679b3199a365397ab82154f1628
k0pernicus <dev@carette.xyz>
2026-04-21 08:24:45 +0200

    Updated gridworld td_lambda function and several script improvement

--- gridworld/gridworld.py
+++ gridworld/gridworld.py
@@ -20,25 +20,26 @@ REWARDS = [
 ]
 
 WORLD_DIMENSIONS = (len(WORLD), len(WORLD[0]))
+_W_H, _W_W = WORLD_DIMENSIONS[0], WORLD_DIMENSIONS[1]
 
 # Basic checks
-assert(len(REWARDS) == len(WORLD)) # Ensures the WORLD and REWARDS nb of rows is the same
+assert(len(REWARDS) == _W_H) # Ensures the WORLD and REWARDS nb of rows is the same
 assert(len(set(map(lambda row: len(row), WORLD))) == 1) # Ensures the WORLD nb of columns is the same per row
-assert(len(REWARDS[0]) == len(WORLD[0])) # Ensures the WORLD and REWARDS nb of columns is the same
+assert(len(REWARDS[0]) == _W_W) # Ensures the WORLD and REWARDS nb of columns is the same
 
 def policy_evaluation(iterations, gamma):
     values = np.zeros(WORLD_DIMENSIONS, dtype=float)
-    values[4][4] = REWARDS[4][4]
+    values[_W_H - 1][_W_W - 1] = REWARDS[_W_H - 1][_W_W - 1]
 
     for i in range(iterations):
         new_values = np.copy(values)
-        for r in range(len(WORLD)):
-            for c in range(len(WORLD[0])):
+        for r in range(_W_H):
+            for c in range(_W_W):
                 if WORLD[r][c] == OUTPUT: continue
                 possible_neighbors = [
                     (r-1, c), (r+1, c), (r, c-1), (r, c+1)
                 ]
-                possible_neighbors = list(filter(lambda c: c[0] >= 0 and c[0] < len(WORLD) and c[1] >= 0 and c[1] < len(WORLD[0]), possible_neighbors))
+                possible_neighbors = list(filter(lambda c: c[0] >= 0 and c[0] < _W_H and c[1] >= 0 and c[1] < _W_W, possible_neighbors))
                 neighbor_rewards = list(map(lambda c: values[c[0]][c[1]], possible_neighbors))
                 new_values[r][c] = REWARDS[r][c] + gamma * (sum(neighbor_rewards) / len(neighbor_rewards))
 
@@ -56,13 +57,13 @@ def value_iteration(iterations, gamma):
 
     for i in range(iterations):
         new_values = np.copy(values)
-        for r in range(len(WORLD)):
-            for c in range(len(WORLD[0])):
+        for r in range(_W_H):
+            for c in range(_W_W):
                 if WORLD[r][c] == OUTPUT: continue
                 possible_neighbors = [
                     (r-1, c), (r+1, c), (r, c-1), (r, c+1)
                 ]
-                possible_neighbors = list(filter(lambda c: c[0] >= 0 and c[0] < len(WORLD) and c[1] >= 0 and c[1] < len(WORLD[0]), possible_neighbors))
+                possible_neighbors = list(filter(lambda c: c[0] >= 0 and c[0] < _W_H and c[1] >= 0 and c[1] < _W_W, possible_neighbors))
                 neighbor_rewards = list(map(lambda c: values[c[0]][c[1]], possible_neighbors))
                 new_values[r][c] = REWARDS[r][c] + gamma * max(neighbor_rewards) # here, we take the best value
 
@@ -85,8 +86,8 @@ def td_lambda(episodes, learning_rate, gamma, lambda_p):
 
     for i in range(episodes):
         # Start the agent at random place, each time you run a new episode
-        r, c = np.random.randint(0, len(WORLD)), np.random.randint(0, len(WORLD[0]))
-        if r == c == (len(WORLD[0]) - 1): c = np.random.random_integers(0, len(WORLD[0]) - 1) # Do not start with the OUTPUT
+        r, c = np.random.randint(0, _W_H), np.random.randint(0, _W_W)
+        if r == c == (_W_W - 1): c = np.random.randint(0, _W_W - 1) # Do not start with the OUTPUT
         eligibility_traces = np.zeros(WORLD_DIMENSIONS, dtype=float) # keep a trace of all footprints of the agent
         while True:
             if WORLD[r][c] == OUTPUT: break # begin another episode
@@ -95,7 +96,7 @@ def td_lambda(episodes, learning_rate, gamma, lambda_p):
             possible_neighbors = [
                 (r-1, c), (r+1, c), (r, c-1), (r, c+1)
             ]
-            possible_neighbors = list(filter(lambda c: c[0] >= 0 and c[0] < len(WORLD) and c[1] >= 0 and c[1] < len(WORLD[0]), possible_neighbors))
+            possible_neighbors = list(filter(lambda c: c[0] >= 0 and c[0] < _W_H and c[1] >= 0 and c[1] < _W_W, possible_neighbors))
             np.random.shuffle(possible_neighbors)
             nr, nc = possible_neighbors[0]
 
@@ -103,12 +104,12 @@ def td_lambda(episodes, learning_rate, gamma, lambda_p):
             td_error = t_reward + gamma * values[nr][nc] - values[r][c]
             eligibility_traces[r][c] += 1
 
-            r, c = nr, nc
+            for wr in range(_W_H):
+                for wc in range(_W_W):
+                    values[wr][wc] += learning_rate * td_error * eligibility_traces[wr][wc] # update the value
+                    eligibility_traces[wr][wc] *= lambda_p * gamma # fade the footprints
 
-            for wr in range(len(WORLD)):
-                for wc in range(len(WORLD[0])):
-                    values[wr][wc] = values[wr][wc] + learning_rate * td_error * eligibility_traces[wr][wc] # update the value
-                    eligibility_traces[wr][wc] = eligibility_traces[wr][wc] * lambda_p * gamma # fade the footprints
+            r, c = nr, nc
 
     return values
 
